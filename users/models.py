c7331a6436148f42bc4bd1c1e2619ac6876cc988
ktravor20-dev <ktravor20@gmail.com>
2026-03-11 14:32:59 -0700

    'customuser implemented'

--- users/models.py
+++ users/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class CustomUser(AbstractUser):
+    class Role(models.TextChoices):
+        STUDENT='STUDENT','student'
+        INTERN_SUPERVISOR='INTERN_SUPERVISOR','intern_supervisor'
+        ACADEMIC_SUPERVISOR='ACADEMIC_SUPERVISOR','academic_supervisor'
+        COMPANY_MANAGER='COMPANY_MANAGER','company_manager'
+        SYSTEM_ADMINSTRATOR='SYSTEM_ADMINSTRATOR','system_adminstrator'
+    role=models.CharField(max_length=30,choices=Role.choices,default=Role.STUDENT)    
+    def __str__(self):
+        return self.username
