cf3bbb70b2146078d3935cf2808fed1b35ab6512
Farid Kibirige <kib5farid@gmail.com>
2026-04-08 23:29:27 +0300

    added 2 APIs

--- users/views.py
+++ users/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .serializer import WeeklyLogsSerializer, UserSerializer
-from .models import WeeklyLogs
+from .serializer import WeeklyLogsSerializer, UserSerializer, internshipPlacementsSerializer,StudentSerializer
+from .models import WeeklyLogs, internshipPlacements, Student
 
 # Create your views here.
 @api_view(['POST'])
@@ -15,12 +15,8 @@ def register(request):
          return Response(serializer.errors, status=400)
 
 
-
-
-
-
-
 @api_view(['GET','POST'])
+
 def get_weekly_logs(request):
     if request.method == 'GET':
      logs= WeeklyLogs.objects.all()
@@ -35,3 +31,31 @@ def get_weekly_logs(request):
           return Response(serializer.errors, status=400)
        
 
+@api_view(['GET','POST'])
+def get_internship_Placement(request):
+   if request.method =='GET':
+      placement = internshipPlacements.objects.all()
+      serializer = internshipPlacementsSerializer(placement, many=True)
+      return Response(serializer.data)
+   else:
+      serializer=internshipPlacementsSerializer(data=request.data)
+      if serializer.is_valid():
+         serializer.save()
+         return Response(serializer.data, status=201)
+      else:
+         return Response(serializer.errors, status=400)
+      
+@api_view(['GET','POST'])
+def get_Student(request):
+   if request.method=='GET':
+      stdnt=Student.objects.all()
+      serializer=StudentSerializer(stdnt, many=True)
+      return Response(serializer.data)
+   else:
+      serializer=StudentSerializer(data=request.data)
+      if serializer.is_save():
+         serializer.save()
+         return Response(serializer.data, status=201)
+      else:
+         return Response(serializer.errors, status=400)
+      
