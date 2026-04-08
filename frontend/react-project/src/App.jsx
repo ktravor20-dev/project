f8368d9fde9b1a22da9caa2942a1103f36fb6b20
ktravor20-dev <ktravor20@gmail.com>
2026-04-08 14:26:01 -0700

    improved the log in

--- frontend/react-project/src/App.jsx
+++ frontend/react-project/src/App.jsx
@@ -9,6 +9,7 @@ function App() {
             <Routes>
                 <Route path='/' element={<Login />}/>
                 <Route path='/register' element={<RegisterUser/>}/>
+                <Route path='/login' element={<Login/>}/>
                 
                 <Route path='/weeklylogs' element={<Weeklogs/>}/>
             </Routes>
