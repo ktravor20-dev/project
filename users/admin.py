761ae1b7d1e73ada0580b8637767714bb9906dad
GAD <chemutai.gad@students.mak.ac.ug>
2026-05-05 15:16:27 +0300

    i have registered the studentlognotificatin model to the the admin site

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import CustomUser,WeeklyLogs,internshipPlacements,Student,internSupervisor ,academicSupervisor,Studentlog,supervisorlog
+from .models import CustomUser,WeeklyLogs,internshipPlacements,Student,internSupervisor ,academicSupervisor,Studentlog,supervisorlog,StudentlogNotification
 
 # Register your models here.
 admin.site.register(CustomUser)
@@ -10,3 +10,4 @@ admin.site.register(internSupervisor)
 admin.site.register(academicSupervisor)
 admin.site.register(Studentlog)
 admin.site.register(supervisorlog)
+admin.site.register(StudentlogNotification)
