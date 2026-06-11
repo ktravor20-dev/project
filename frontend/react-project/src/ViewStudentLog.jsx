e3393a8894a37a46e4070130f5b67c2ac3be213e
ktravor20-dev <ktravor20@gmail.com>
2026-06-11 13:20:12 -0700

    modified the student log

--- frontend/react-project/src/ViewStudentLog.jsx
+++ frontend/react-project/src/ViewStudentLog.jsx
@@ -85,7 +85,8 @@ function ViewStudentLog(){
                     
             });
             toast.success('Status updated successfully');
-            navigate('/supervisorDashboard/viewstudentlog');
+            setlogs(logs.map(log => log.id === id ? { ...log, is_read: true } : log));
+            
             
 
         }catch(error){
